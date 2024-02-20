d498788c7a9fc64609f21f895c9aabf796552fc5
TheRohit <rohitmeshram000@gmail.com>
2024-02-20 10:17:34 +0530

    chore: wip

--- src/components/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { Card } from "@tremor/react";
+
 interface AnalyticsDashboardProps {}
 
 const AnalyticsDashboard = ({}: AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6 ">
-      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6"></div>
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
+        <Card className="w-full mx-auto max-w-xs">
+          <p className=""></p>
+        </Card>
+      </div>
     </div>
   );
 };
